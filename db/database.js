9a2a40c7a400c7e4d9ae77a979a5dcfdcfb832ad
Songysp <hyeon9502@naver.com>
2024-05-09 15:40:12 +0900

    Mongodb 적용

--- db/database.js
+++ db/database.js
@@ -1,10 +1,18 @@
 import { config } from '../config.js';
-import SQ from 'sequelize';
+import MongoDB from 'mongodb';
 
-const{ host, user, database, password, port } = config.db;
+let db;
 
-export const sequelize = new SQ.Sequelize(database, user, password, {
-    host,
-    dialect: 'mysql',
-    logging: false,
-});
+export async function connectDB() {
+    return await MongoDB.MongoClient.connect(config.db.host)
+        .then((client) => db = client.db());
+}
+
+export function getUsers(){
+    return db.collection('users');
+}
+
+export function getTweets(){
+    return db.collection('tweets');
+}
+
